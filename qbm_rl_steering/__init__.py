8a82839505962db0d24c616605d72a9bd0d527d5
Verena Kain <verena.kain@cern.ch>
2021-02-01 20:51:49 +0100

    First commit

---
+++ qbm_rl_steering/__init__.py
@@ -0,0 +1,6 @@
+"""
+Documentation for the qbm_rl_steering package
+
+"""
+
+__version__ = "0.0.1.dev0"
---
+++ qbm_rl_steering/tests/test_qbm_rl_steering.py
@@ -0,0 +1,10 @@
+"""
+High-level tests for the  package.
+
+"""
+
+import qbm_rl_steering
+
+
+def test_version():
+    assert qbm_rl_steering.__version__ is not None
