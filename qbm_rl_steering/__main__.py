7c9fb1c7cc0ebf68eb0444d19ae7bdfd0b718c1b
Michael Schenk <michuschenk@gmail.com>
2021-02-02 16:15:54 +0100

    Implement main function: test environment and run DQN learning (stable-baselines3)

--- qbm_rl_steering/__main__.py
+++ qbm_rl_steering/__main__.py
@@ -1,7 +1,34 @@
+from environment.env_desc import TargetSteeringEnv
+from environment.helpers import plot_response, run_random_trajectories
+from stable_baselines3 import DQN
+from stable_baselines3.dqn import MlpPolicy
 
-
-
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 if __name__ == "__main__":
-    print('Hallo Michael')
+    # Transfer line environment
+    my_env = TargetSteeringEnv()
+
+    # To understand environment better, plot response and test random
+    # action-taking
+    plot_response(my_env)
+    run_random_trajectories(my_env)
+
+    # DQN: state and action is discrete: see here: https://stable-baselines3.readthedocs.io/en/master/modules/dqn.html
+    model = DQN(MlpPolicy, my_env, verbose=1)
+    model.learn(total_timesteps=2000)
+
+    # Plot log taking while learning DQN
+    log = np.array(my_env.log)
+    fig, axs = plt.subplots(3, 1, sharex=True, figsize=(7, 6))
+    labels = ('state', 'action', 'reward')
+    for i in range(3):
+        axs[i].plot(log[:, i])
+        axs[i].set_ylabel(labels[i])
+        # for j in range(n_epochs+1):
+        #     axs[i].axvline(j * n_episodes, color='red', ls='--')
+    axs[-1].set_xlabel('Iterations')
+    plt.tight_layout()
+    plt.show()
