2f4eea8a3c79de4675a7d9e2733a53d818e8a53e
Michael Schenk <michuschenk@gmail.com>
2021-02-02 16:05:18 +0100

    Fix formatting

--- qbm_rl_steering/environment/env_desc.py
+++ qbm_rl_steering/environment/env_desc.py
@@ -4,29 +4,28 @@ import math
 from scipy.integrate import quad
 from math import pi, exp
 
-class twissElement():
 
-    def __init__(self,beta,alpha,d,mu):
+class TwissElement():
 
+    def __init__(self, beta, alpha, d, mu):
         self.beta = beta
         self.alpha = alpha
         self.mu = mu
 
-def transport(element1,element2,x,px):
+
+def transport(element1, element2, x, px):
     mu = element2.mu - element1.mu
     alpha1 = element1.alpha
     alpha2 = element2.alpha
     beta1 = element1.beta
     beta2 = element2.beta
 
+    m11 = math.sqrt(beta2 / beta1) * (math.cos(mu) + alpha1 * math.sin(mu))
+    m12 = math.sqrt(beta1 * beta2) * math.sin(mu)
+    m21 = ((alpha1 - alpha2) * math.cos(mu) - (1 + alpha1 * alpha2) * math.sin(mu)) / math.sqrt(beta1 * beta2)
+    m22 = math.sqrt(beta1 / beta2) * (math.cos(mu) - alpha2 * math.sin(mu))
 
-    m11 = math.sqrt(beta2/beta1)*(math.cos(mu)+alpha1*math.sin(mu))
-    m12 = math.sqrt(beta1*beta2)*math.sin(mu)
-    m21 = ((alpha1-alpha2)*math.cos(mu)-(1+alpha1*alpha2)*math.sin(mu))/math.sqrt(beta1*beta2)
-    m22 = math.sqrt(beta1/beta2)*(math.cos(mu)-alpha2*math.sin(mu))
-
-    return m11*x+m12*px, m21*x+m22*px
-
+    return m11 * x + m12 * px, m21 * x + m22 * px
 
 
 class TargetSteeringEnv(gym.Env):
@@ -49,21 +48,19 @@ class TargetSteeringEnv(gym.Env):
         pass
 
 
-    def _get_reward(self,beam_pos):
-
+    def _get_reward(self, beam_pos):
         emittance = 1.1725E-08
-        sigma = math.sqrt(self.target.beta*emittance)
-        self.intensity_on_target = quad(lambda x: 1 / (sigma * (2 * pi) ** 0.5) * exp((x-beam_pos) ** 2 / (-2 * sigma ** 2)), -3*sigma,3*sigma)
+        sigma = math.sqrt(self.target.beta * emittance)
+        self.intensity_on_target = quad(
+            lambda x: 1 / (sigma * (2 * pi) ** 0.5) * exp((x - beam_pos) ** 2 / (-2 * sigma ** 2)), -3 * sigma,
+            3 * sigma)
 
         reward = self.intensity_on_target[0]
         return reward
 
-    #
-    def _get_pos_at_bpm_target(self,total_angle):
-        x_bpm, px_bpm = transport(self.mssb, self.bpm1, self.x0,total_angle)
-        x_target,px_target = transport(self.mssb, self.target, self.x0,total_angle)
+    def _get_pos_at_bpm_target(self, total_angle):
+        x_bpm, px_bpm = transport(self.mssb, self.bpm1, self.x0, total_angle)
+        x_target, px_target = transport(self.mssb, self.target, self.x0, total_angle)
 
         reward = self._get_reward(x_target)
-        return x_bpm,reward
-
-
+        return x_bpm, reward
