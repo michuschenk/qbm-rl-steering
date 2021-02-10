a181a05a9d693fd0b5ef938cc9e5b61d197f23a5
Michael Schenk <michuschenk@gmail.com>
2021-02-10 16:08:49 +0100

    Implement multi-step learning with episodes and epsilon-greedy decay, add comments and type hints

--- qbm_rl_steering/agents/qbmq.py
+++ qbm_rl_steering/agents/qbmq.py
@@ -1,4 +1,3 @@
-import gym
 import random
 from qbm_rl_steering.environment.env_desc import TargetSteeringEnv
 import qbm_rl_steering.agents.qbmq_utils as utl
@@ -6,6 +5,8 @@ import qbm_rl_steering.agents.qbmq_utils as utl
 from neal import SimulatedAnnealingSampler
 import numpy as np
 import math
+import tqdm
+
 from typing import Union
 
 
@@ -35,14 +36,19 @@ def convert_bits(vec: Union[tuple, np.ndarray], mapping: dict = {0: -1}) \
     return tuple(vec)
 
 
-def create_visible_iterable(state: np.ndarray, action_index: int) -> tuple:
+def create_visible_iterable(state: np.ndarray, action: int) -> tuple:
     """ Take state (e.g. directly from environment), and action_index (
     following env.action_map), and concatenate them to the visible_iterable
     tuple required for the create_general_Q_from(..) function. This also
     converts all 0s appearing in state and action_index (once made binary) to
-    -1s. """
+    -1s.
+    :param state: state in binary-encoded vector, as obtained directly from
+    the environment (either through .reset(), or .step())
+    :param action: index of action as used in environment
+    :return tuple, concatenation of state and action, following the
+    convention of Mircea's code on QBM. """
     s = convert_bits(state)
-    a = convert_bits(make_action_binary(action_index))
+    a = convert_bits(make_action_binary(action))
     return s + a
 
 
@@ -51,7 +57,19 @@ class QFunction(object):
                  n_bits_action_space: int, possible_actions: list,
                  replica_count: int, average_size: int, big_gamma: float,
                  beta: float) -> None:
+        """ Implementation of the Q function using DWAVE neal sampler.
+        :param n_bits_observation_space: number of bits used to encode
+        observation space of environment
+        :param n_bits_action_space: number of bits required to encode the
+        actions that are possible in the given environment
+        :param possible_actions: list of possible action indices
+        :param replica_count: ?
+        :param average_size: ?
+        :param big_gamma: QBM param., see paper
+        :param beta: QBM param., see paper
+        """
 
+        # TODO: rename Q_hh, Q_vh into w_hh, w_vh
         self.Q_hh = dict()
         self.Q_vh = dict()
         self.replica_count = replica_count
@@ -65,9 +83,8 @@ class QFunction(object):
         self.possible_actions = possible_actions
         self._initialise_weights()
 
-    # TODO: define epsilon, that can decay over time. predict returns random or
-    #  Q value
-    def _initialise_weights(self):
+    def _initialise_weights(self) -> None:
+        """ Initialise the weights of the Q function network. """
         for i, ii in zip(tuple(range(4)), tuple(range(8, 12))):
             for j, jj in zip(tuple(range(4, 8)), tuple(range(12, 16))):
                 self.Q_hh[(i, j)] = 2 * random.random() - 1
@@ -86,17 +103,17 @@ class QFunction(object):
                     self.n_bits_observation_space + self.n_bits_action_space):
                 self.Q_vh[(i, j,)] = 2 * random.random() - 1
 
-    def calculate_F(self, state, action):
+    def calculate_F(self, state: np.ndarray, action: int) -> tuple:
         """ Based on state and chosen action, calculate the free energy,
         samples and vis_iterable.
         :param state: state the environment is in (binary vector, directly
         obtained from either env.reset(), or env.step())
         :param action: chosen action (index)
         :return free energy, samples, and vis_iterable """
-        vis_iterable = create_visible_iterable(state=state, action_index=action)
+        vis_iterable = create_visible_iterable(state=state, action=action)
 
-        general_Q = utl.create_general_Q_from(Q_hh=self.Q_hh, Q_vh=self.Q_vh,
-                                              visible_iterable=vis_iterable)
+        general_Q = utl.create_general_Q_from(
+            Q_hh=self.Q_hh, Q_vh=self.Q_vh, visible_iterable=vis_iterable)
 
         samples = list(SimulatedAnnealingSampler().sample_qubo(
             general_Q, num_reads=self.sample_count).samples())
@@ -112,7 +129,7 @@ class QFunction(object):
 
         return free_energy, samples, vis_iterable
 
-    def calculate_and_predict(self, state, epsilon):
+    def calculate_and_predict(self, state: np.ndarray, epsilon: float) -> tuple:
         """ Get a2 = argmax_a Q(s2, a), but following epsilon-greedy policy
         :param state: state the environment is in (binary vector, directly
         obtained from either env.reset(), or env.step())
@@ -148,90 +165,121 @@ class QFunction(object):
             return (max_dict['action'], max_dict['free_energy'],
                     max_dict['samples'], max_dict['vis_iterable'])
 
-    def update_weights(self):
-        pass
-
 
 class QBMQN(object):
-    def __init__(self, env: TargetSteeringEnv, replica_count, average_size,
-                 big_gamma, beta):
+    def __init__(self, env: TargetSteeringEnv, replica_count: int,
+                 average_size: int, big_gamma: float = 0.5, beta: float = 2.0,
+                 learning_rate: float = 1e-4,
+                 exploration_fraction: float = 0.8,
+                 exploration_initial_eps: float = 1.0,
+                 exploration_final_eps: float = 0.05,
+                 small_gamma: float = 0.99) -> None:
+        """ Implementation of the QBM Q learning agent, following the paper:
+        https://arxiv.org/pdf/1706.00074.pdf
+        :param env: OpenAI gym environment
+        :param replica_count: ?
+        :param average_size: ?
+        :param big_gamma: QBM param., see paper
+        :param beta: QBM param., 'temperature', see paper
+        :param learning_rate: RL. param., learning rate for update of weights
+        :param exploration_fraction: RL param., fraction of total number of
+        time steps over which the epsilon decays
+        :param exploration_initial_eps: RL param., initial epsilon for
+        epsilon-greedy param.
+        :param exploration_final_eps: RL param., final epsilon for
+        epsilon-greedy param.
+        :param small_gamma: RL param., discount factor """
         self.env = env
+
+        # Learning parameters
+        self.learning_rate = learning_rate
+        self.small_gamma = small_gamma
+        self.exploration_fraction = exploration_fraction
+        self.exploration_initial_eps = exploration_initial_eps
+        self.exploration_final_eps = exploration_final_eps
+
+        # Q function
         n_bits_observation_space = env.n_bits_observation_space
-        # how many bits do we need to describe the action space?
         n_bits_action_space = math.ceil(math.log2(env.action_space.n))
         possible_actions = [i for i in range(env.action_space.n)]
-
         self.q_function = QFunction(
             n_bits_observation_space, n_bits_action_space, possible_actions,
             replica_count, average_size, big_gamma, beta)
 
-    def learn(self, n_iterations):
-        pass
-        # state_1 = self.env.reset()
-        # all_possible_actions = env.something
-        # for i in range(n_iterations):
-        #     action_1 = self.q_function.predict(state_1,all_possible_actions) # --> current_F,
-        #     state_2,reward, done,_ = env.step(action_1)
-        #     action_2 = self.q_function.predict(state_2,all_possible_actions) # --> future_F
+    def learn(self, total_timesteps: int) -> None:
+        """ Train the agent for the specified number of iterations.
+        :param total_timesteps: number of training steps """
+        epsilon = self._get_epsilon_schedule(total_timesteps)
 
-        # check what is required for weight update
-        # check how to get handle on current and future F.
-        # at the end state_1 = state_2
+        state_1 = env.reset()
+        for it in tqdm.trange(total_timesteps):
+            # Step 1: given the current_state, pick an action randomly (this is
+            # to compute current_F). This corresponds to (s1, a1).
+            # TODO: is this really what we have to do? Random action here?
+            action_1 = env.action_space.sample()
+
+            current_F, current_samples, current_vis_iterable = (
+                self.q_function.calculate_F(state_1, action_1)
+            )
+
+            # Step 2: take the step in the environment
+            state_2, reward_1, done, _ = env.step(action=action_1)
+
+            # Step 3: get action_2 = argmax_act Q(state_2, act)
+            # Need to take max Q, resp. min. F, or random action if
+            # epsilon-greedy is fulfilled. Do I really need to loop through
+            # all the actions to calculate the Q values for every action to
+            # then pick the argmax  Q?
+            action_2, future_F, future_samples, future_vis_iterable = (
+                self.q_function.calculate_and_predict(state_2, epsilon[it])
+            )
+
+            # Step 4: update weights
+            self.q_function.Q_hh, self.q_function.Q_vh = (
+                utl.update_weights(
+                    self.q_function.Q_hh,
+                    self.q_function.Q_vh,
+                    current_samples,  # why current_samples?
+                    reward_1,
+                    future_F,
+                    current_F,
+                    current_vis_iterable,
+                    self.learning_rate,
+                    self.small_gamma)
+            )
+
+            state_1 = state_2
+            if done:
+                state_1 = env.reset()
+
+    def _get_epsilon_schedule(self, total_timesteps: int) -> np.ndarray:
+        """ Define epsilon schedule as linear decay between time step 0 and
+        time step exploration_fraction * total_timesteps, starting from
+        exploration_initial_eps and ending at exploration_final_eps.
+        :param total_timesteps: total number of training steps
+        :return epsilon array including decay """
+        n_steps_decay = int(self.exploration_fraction * total_timesteps)
+        eps_step = (
+            (self.exploration_final_eps - self.exploration_initial_eps) /
+            n_steps_decay)
+        eps_decay = np.arange(
+            self.exploration_initial_eps, self.exploration_final_eps, eps_step)
+        eps = np.ones(total_timesteps) * self.exploration_final_eps
+        eps[:n_steps_decay] = eps_decay
+        return eps
 
 
 if __name__ == "__main__":
     debug = True
 
+    # TODO: Logging needs to be changed potentially?
     # TODO: where should these variables go? Into QBMQN?
     replica_count = 10
     average_size = 50
-    sample_count = replica_count * average_size
-
-    beta = 2.
-    big_gamma = 0.5
-
-    learning_rate = 1e-3
-    small_gamma = 0.98
-    epsilon = 0.1
 
     env = TargetSteeringEnv(n_bits_observation_space=8)
-    qbmqn = QBMQN(env, replica_count, average_size, big_gamma, beta)
-
-    # Initialize environment
-    current_state = env.reset()
-
-    # Step 1: given the current_state, pick an action randomly (this is to
-    # compute current_F). This corresponds to (s1, a1).
-    # TODO: is this really what we have to do? Random action here?
-    state_1 = current_state
-    action_1 = env.action_space.sample()
-
-    current_F, current_samples, current_vis_iterable = (
-        qbmqn.q_function.calculate_F(state_1, action_1)
-    )
-
-    # Step 2: take the step in the environment
-    state_2, reward_1, done, _ = env.step(action=action_1)
-
-    # Step 3: get action_2 = argmax_act Q(state_2, act)
-    # Need to take max Q, resp. min. F, or random action if epsilon-greedy is
-    # fulfilled. Do I really need to loop through all the actions to
-    # calculate the Q values for every action to then pick the argmax  Q?
-    action_2, future_F, future_samples, future_vis_iterable = (
-        qbmqn.q_function.calculate_and_predict(state_2, epsilon)
-    )
-
-    # Step 4: update weights
-    # TODO: Rename Q_hh, Q_vh to w_hh, w_vh
-    qbmqn.q_function.Q_hh, qbmqn.q_function.Q_vh = (
-        utl.update_weights(
-            qbmqn.q_function.Q_hh,
-            qbmqn.q_function.Q_vh,
-            current_samples,  # why current_samples?
-            reward_1,
-            future_F,
-            current_F,
-            current_vis_iterable,
-            learning_rate,
-            small_gamma)
-    )
+    agent = QBMQN(env, replica_count=replica_count, average_size=average_size,
+                  big_gamma=0.5, beta=2., exploration_fraction=0.8,
+                  exploration_initial_eps=1.0, exploration_final_eps=0.,
+                  small_gamma=0.98, learning_rate=1e-3)
+    agent.learn(total_timesteps=10)
