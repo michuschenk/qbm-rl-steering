227cb077a40792489df9a530793356ab41d6b9ba
Michael Schenk <michuschenk@gmail.com>
2021-04-06 18:15:14 +0200

    Add test of env. with SAC from stable baselines3

---
+++ qbm_rl_steering/actor_critic_test.py
@@ -0,0 +1,47 @@
+from stable_baselines3.common.noise import NormalActionNoise
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from qbm_rl_steering.environment.env_desc import TargetSteeringEnv
+from stable_baselines3 import SAC
+
+env = TargetSteeringEnv()
+noise = NormalActionNoise(np.array([0.1]), np.array([0.01]))
+model = SAC("MlpPolicy", env, verbose=1, action_noise=noise, gamma=0.85,
+            learning_rate=1e-3)
+model.learn(2000, log_interval=4)
+
+
+n_evals = 100
+init_rewards = np.zeros(n_evals)
+final_rewards = np.zeros(n_evals)
+
+ctr = 0
+obs = env.reset()
+obs = np.array([obs])
+_, intensity = env.get_pos_at_bpm_target(env.mssb_angle)
+init_rewards[ctr] = env.get_reward(intensity)
+# print('initial reward', env.get_reward(intensity))
+
+while ctr < n_evals:
+    action, _states = model.predict(obs, deterministic=True)
+    obs, reward, done, info = env.step(action)
+    obs = np.array([obs])
+    # print('reward', reward)
+    if done:
+        final_rewards[ctr] = reward
+        # print('================ done!')
+        obs = env.reset()
+        obs = np.array([obs])
+        _, intensity = env.get_pos_at_bpm_target(env.mssb_angle)
+        # print('initial reward', env.get_reward(intensity))
+        init_rewards[ctr] = env.get_reward(intensity)
+        ctr += 1
+
+
+plt.plot(init_rewards, label='init')
+plt.plot(final_rewards, label='final')
+plt.legend()
+plt.show()
+
