Introduce action and state space scaling for DDPG. Fix env. plotting

qbm_rl_steering/agents/actor_critic.py
@@ -15,6 +15,8 @@ try:
 except ImportError as err:
     print(err)
 
+from qbm_rl_steering.utils.helpers import plot_log
+
 
 class Memory:
     """A FIFO experiene replay buffer.
@@ -42,14 +44,14 @@ class Memory:
         return self.states[idxs], self.actions[idxs], self.rewards[idxs], self.next_states[idxs], self.dones[idxs]
 
 class ClassicACAgent(object):
-    def __init__(self, state_dim, action_n, act_limit):
+    def __init__(self, state_dim, action_n, act_limit, GAMMA):
         # env intel
         self.action_n = action_n
         self.state_dim = state_dim
         self.state_n = state_dim[0]
         # constants
         self.ACT_LIMIT = act_limit  # requiered for clipping prediciton aciton
-        self.GAMMA = 0.99  # discounted reward factor
+        self.GAMMA = GAMMA  # discounted reward factor
         self.TAU = 1  # soft update factor
         self.BUFFER_SIZE = int(1e6)
         self.BATCH_SIZE = 50  # training batch size.
@@ -89,7 +91,7 @@ class ClassicACAgent(object):
         q =self.critic([y_true,y_pred, self.dummy_Q_target_prediction_input, self.dummy_dones_input])
         return -K.backend.mean(q)
 
-    def get_action(self, states, noise=None,episode = 1):
+    def get_action(self, states, noise=None, episode=1):
 
         if noise is None: noise = self.ACT_NOISE_SCALE
         if len(states.shape) == 1: states = states.reshape(1, -1)
@@ -103,7 +105,6 @@ class ClassicACAgent(object):
         return self.actor_target.predict_on_batch(states)
 
     def train_actor(self, states, actions):
-
         self.actor.train_on_batch(states, states)#Q_predictions)
 
 
@@ -182,17 +183,15 @@ from qbm_rl_steering.environment.env_desc import TargetSteeringEnv
 if __name__ == "__main__":
 
     GAMMA = 0.85
-    EPOCHS = 50
-    MAX_EPISODE_LENGTH = 40
+    EPOCHS = 16
+    MAX_EPISODE_LENGTH = 10
     START_STEPS = 10
     INITIAL_REW = 0
 
-    env = TargetSteeringEnv()
-    env.action_scale = 1e-5
-
+    env = TargetSteeringEnv(max_steps_per_episode=MAX_EPISODE_LENGTH)
     agent = ClassicACAgent(
         env.observation_space.shape, env.action_space.shape[0],
-        max(env.action_space.high))
+        max(env.action_space.high), GAMMA)
 
     state, reward, done, ep_rew, ep_len, ep_cnt = env.reset(), INITIAL_REW, \
                                                   False, [[]], 0, 0
@@ -235,7 +234,8 @@ if __name__ == "__main__":
             for _ in range(ep_len):
                 agent.train()
 
-            state, reward, done, ep_ret, ep_len = env.reset(), INITIAL_REW, False, 0, 0
+            state, reward, done, ep_ret, ep_len = (
+                env.reset(), INITIAL_REW, False, 0, 0)
 
             _, intensity = env.get_pos_at_bpm_target(env.mssb_angle)
             ep_rew[-1].append(env.get_reward(intensity))
@@ -243,17 +243,42 @@ if __name__ == "__main__":
     init_rewards = []
     rewards = []
     reward_lengths = []
-    for episode in ep_rew:
+    for episode in ep_rew[:-1]:
         if(len(episode) > 0):
             rewards.append(episode[-1])
             init_rewards.append(episode[0])
             reward_lengths.append(len(episode)-1)
+    print('Total number of interactions:', np.sum(reward_lengths))
 
-    fig, axs = plt.subplots(2, 1, constrained_layout=True)
-    ax = axs[0]
-    ax.plot(reward_lengths)
+    plot_log(env, fig_title='Training')
+    # fig, axs = plt.subplots(2, 1, constrained_layout=True, figsize=(8, 6))
+    # axs[0].plot(reward_lengths)
+    # axs[0].axhline(env.max_steps_per_episode, c='k', ls='-',
+    #                label='Max. # steps')
+    # axs[0].set_ylabel('# steps per episode')
+    # axs[0].set_ylim(0, env.max_steps_per_episode + 0.5)
+    # axs[0].legend(loc='upper right')
+    #
+    # axs[1].plot(init_rewards, c='r', marker='.', label='initial')
+    # axs[1].plot(rewards, c='forestgreen', marker='x', label='final')
+    # axs[1].legend(loc='lower right')
+    # axs[1].set_xlabel('Episode')
+    # axs[1].set_ylabel('Reward')
+    # plt.show()
 
-    ax = axs[1]
-    ax.plot(init_rewards)
-    ax.plot(rewards)
-    plt.show()
+
+    # Agent evaluation
+    n_episodes_eval = 50
+    episode_counter = 0
+
+    env = TargetSteeringEnv(max_steps_per_episode=MAX_EPISODE_LENGTH)
+    while episode_counter < n_episodes_eval:
+        state = env.reset(init_outside_threshold=True)
+        while True:
+            a = agent.get_action(state, noise=0)
+            state, reward, done, _ = env.step(a)
+            if done:
+                episode_counter += 1
+                break
+
+    plot_log(env, fig_title='Evaluation')

qbm_rl_steering/environment/env_desc.py
@@ -45,8 +45,7 @@ def transport(element1: TwissElement, element2: TwissElement, x: float,
 
 
 class TargetSteeringEnv(gym.Env):
-    def __init__(self, max_steps_per_episode: int = 20,
-                 action_scale: float = 1e-5) -> None:
+    def __init__(self, max_steps_per_episode: int = 20) -> None:
         """
         :param max_steps_per_episode: max number of steps we allow agent to
         'explore' per episode. After this number of steps, episode is aborted.
@@ -67,7 +66,8 @@ class TargetSteeringEnv(gym.Env):
         self.mssb_angle_max = 140e-6  # 140e-6  # (rad)
         self.mssb_angle_min = -self.mssb_angle_max  # (rad)
         self.mssb_angle_margin = 30e-6
-        self.action_scale = action_scale
+        self.action_scale = (2. / (self.mssb_angle_max - self.mssb_angle_min +
+                                   2 * self.mssb_angle_margin))
 
         # BEAM POSITION
         # x0: position at origin, i.e. before entering MSSB
@@ -76,18 +76,24 @@ class TargetSteeringEnv(gym.Env):
         # observation_space given mssb_angle_min, mssb_angle_max
         self.x0 = 0.
         self.state = None  # not set, will be init. with self.reset()
-        x_max, _ = self.get_pos_at_bpm_target(self.mssb_angle_max)
-        x_min, _ = self.get_pos_at_bpm_target(self.mssb_angle_min)
+        x_max_wMargin, _ = self.get_pos_at_bpm_target(self.mssb_angle_max +
+                                                      self.mssb_angle_margin)
+        x_min_wMargin, _ = self.get_pos_at_bpm_target(self.mssb_angle_min -
+                                                      self.mssb_angle_margin)
+        self.state_scale = 2. / (x_max_wMargin - x_min_wMargin)
 
         # GYM REQUIREMENTS
         # Define continuous action space
         self.action_space = gym.spaces.Box(
-            low=np.array([self.mssb_angle_min - self.mssb_angle_margin]),
-            high=np.array([self.mssb_angle_max + self.mssb_angle_margin]))
+            low=np.array([self.mssb_angle_min - self.mssb_angle_margin]) *
+                self.action_scale,
+            high=np.array([self.mssb_angle_max + self.mssb_angle_margin]) *
+                self.action_scale)
 
         # Define continuous observation space
         self.observation_space = gym.spaces.Box(
-            low=np.array([1.05*x_min]), high=np.array([1.05*x_max]))
+            low=np.array([x_min_wMargin * self.state_scale]),
+            high=np.array([x_max_wMargin * self.state_scale]))
 
         # For cancellation when beyond certain number of steps in an episode
         self.step_count = None
@@ -108,11 +114,10 @@ class TargetSteeringEnv(gym.Env):
 
         # Apply action and update environment (get new position at BPM and
         # convert into a binary vector)
-        action *= self.action_scale
-        total_angle = self.mssb_angle + action
+        total_angle = self.mssb_angle + action / self.action_scale
         x_new, intensity = self.get_pos_at_bpm_target(total_angle)
 
-        self.state = np.array([x_new])
+        self.state = np.array([x_new * self.state_scale])
         self.mssb_angle = total_angle
 
         self.step_count += 1
@@ -121,8 +126,8 @@ class TargetSteeringEnv(gym.Env):
         done = bool(
             self.step_count > self.max_steps_per_episode
             or intensity > self.intensity_threshold
-            or x_new > self.observation_space.high
-            or x_new < self.observation_space.low
+            or x_new > (self.observation_space.high / self.state_scale)
+            or x_new < (self.observation_space.low / self.state_scale)
         )
 
         # Keep track of reason for episode abort
@@ -131,8 +136,8 @@ class TargetSteeringEnv(gym.Env):
             done_reason = 0
         elif intensity > self.intensity_threshold:
             done_reason = 1
-        elif (x_new > self.observation_space.high or
-              x_new < self.observation_space.low):
+        elif (x_new > (self.observation_space.high / self.state_scale) or
+              x_new < (self.observation_space.low / self.state_scale)):
             done_reason = 2
         else:
             pass
@@ -141,7 +146,7 @@ class TargetSteeringEnv(gym.Env):
 
         # Interaction log
         self.interaction_logger.log_episode.append(
-            [x, action, reward, x_new, done, done_reason])
+            [x, action / self.action_scale, reward, x_new, done, done_reason])
         if done:
             self.interaction_logger.log_all.append(
                 self.interaction_logger.log_episode)
@@ -203,12 +208,16 @@ class TargetSteeringEnv(gym.Env):
         else:
             self.mssb_angle = self._init_specific_state(init_state)
 
-        x_init, _ = self.get_pos_at_bpm_target(self.mssb_angle)
-        self.state = np.array([x_init])
+        x_init, init_intensity = self.get_pos_at_bpm_target(self.mssb_angle)
+        self.state = np.array([x_init * self.state_scale])
 
         # Logging
         self.step_count = 0
         self.interaction_logger.episode_reset()
+
+        init_reward = self.get_reward(init_intensity)
+        self.interaction_logger.log_episode.append(
+            [x_init, None, init_reward, None, False, None])
 
         return self.state
 