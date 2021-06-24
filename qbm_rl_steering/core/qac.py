1e818dc95b3ac323d4c8f4eab35e3af43935d626
mischenk <michael.schenk@cern.ch>
2021-06-24 15:33:22 +0200

    Implement analytical derivative of effective Hamiltonian, make sure local and target nets start in identical states, increase QBM

--- qbm_rl_steering/core/qac.py
+++ qbm_rl_steering/core/qac.py
@@ -12,7 +12,7 @@ from qbm_rl_steering.core.utils import Memory
 
 
 class QuantumActorCritic:
-    def __init__(self, env: gym.Env, gamma_rl: float):
+    def __init__(self, env: gym.Env, gamma_rl: float, batch_size: int):
         # env intel
         self.env = env
         self.action_n = env.action_space.shape[0]
@@ -23,16 +23,16 @@ class QuantumActorCritic:
         self.gamma_rl = gamma_rl  # discounted reward factor
         self.tau_soft_update = 0.1  # soft update factor
         self.replay_buffer_size = int(1e5)
-        self.replay_batch_size = 10  # training batch size.
-        self.action_noise_scale = 0.2
+        self.replay_batch_size = batch_size  # training batch size.
+        self.action_noise_scale = 0.15
 
         # Critic-related input
         self.n_annealing_steps = 80
-        self.n_annealings_for_average = 40
-        self.learning_rate_critic = 6e-3
+        self.n_annealings_for_average = 30
+        self.learning_rate_critic = 1e-3  #2e-3
 
         # Actor-related input
-        self.init_learning_rate_actor = 1e-3
+        self.init_learning_rate_actor = 5e-4
 
         # create networks
         self.dummy_Q_target_prediction_input = np.zeros(
@@ -44,9 +44,26 @@ class QuantumActorCritic:
         self.actor = self._generate_actor_net()
         self.actor_target = self._generate_actor_net()
 
+        # Make weights of local and target nets identical
+        # Critic
+        for k in self.critic.w_hh.keys():
+            self.critic_target.w_hh[k] = self.critic.w_hh[k]
+        for k in self.critic.w_vh.keys():
+            self.critic_target.w_vh[k] = self.critic.w_vh[k]
+
+        # Actor
+        weights_actor_local = np.array(self.actor.get_weights())
+        self.actor_target.set_weights(weights_actor_local)
+
         # Initialize replay buffer
         self.replay_memory = Memory(
             self.state_dim[0], self.action_n, self.replay_buffer_size)
+
+        # loss tracking
+        self.mu_losses = []
+        self.q_losses = []
+        self.q_before = []
+        self.q_after = []
 
     def _generate_actor_net(self):
         """ Create classical actor neural network. """
@@ -64,6 +81,8 @@ class QuantumActorCritic:
     def get_action(self, states, noise=None, episode=1):
         """ Get batch of proposed actions from the local actor network based
         on batch of input states. Also add noise during training phase. """
+        # print('get_action states.shape', states.shape)
+
         if noise is None:
             noise = self.action_noise_scale
         if len(states.shape) == 1:
@@ -71,16 +90,20 @@ class QuantumActorCritic:
         action = self.actor.predict_on_batch(states)
         if noise != 0:
             action += noise / episode * np.random.randn(self.action_n)
+            # action += noise * np.random.randn(self.action_n)
             action = np.clip(action, -self.action_limit, self.action_limit)
         return action
 
     def get_target_actions(self, states):
         """ Get batch of proposed actions from the target actor network based
         on batch of input states. """
+        # states = np.atleast_2d(states)
         return self.actor_target.predict_on_batch(states)
 
     def train_actor(self, states, actions):
+        # print('before training actor', self.actor.get_weights())
         self.actor.train_on_batch(states, states)
+        # print('after training actor', self.actor.get_weights())
 
     def _generate_critic_net(self):
         """ Initialize QBM with random weights / couplings. Here we also
@@ -113,8 +136,19 @@ class QuantumActorCritic:
         def get_q_value(y_true, y_pred):
             q_value, _, _ = (
                 self.critic.calculate_q_value_on_batch(y_true, y_pred))
+            q_value = np.array(q_value)
+
+            # Numerical derivatives
             dq_over_dstate = self.get_state_derivative(y_true, y_pred)
             dq_over_daction = self.get_action_derivative(y_true, y_pred)
+
+            # q_value, dq_over_dstate, dq_over_daction = \
+            #     self.get_state_action_derivative_analytical(
+            #         y_true, y_pred)
+
+            # print('q_value.shape', q_value.shape)
+            # print('dq_ds.shape', dq_over_dstate.shape)
+            # print('dq_da.shape', dq_over_daction.shape)
 
             return (np.float32(q_value), np.float32(dq_over_dstate),
                     np.float32(dq_over_daction))
@@ -128,44 +162,167 @@ class QuantumActorCritic:
 
         return z, grad
 
-    def get_state_derivative(self, y_true, y_pred, epsilon=0.2):
-        qeps_plus, _, _ = self.critic.calculate_q_value_on_batch(
-            y_true + epsilon, y_pred)
-        qeps_minus, _, _ = self.critic.calculate_q_value_on_batch(
-            y_true - epsilon, y_pred)
-        return np.float_((qeps_plus - qeps_minus) / (2 * epsilon))
+    # def get_state_derivative(self, y_true, y_pred, epsilon=0.2):
+    #     # print('state deriv, plus epsilon')
+    #     qeps_plus, _, _ = self.critic.calculate_q_value_on_batch(
+    #         y_true + epsilon, y_pred)
+    #
+    #     # print('state deriv, minus epsilon')
+    #     qeps_minus, _, _ = self.critic.calculate_q_value_on_batch(
+    #         y_true - epsilon, y_pred)
+    #     return np.atleast_1d(
+    #         np.float_((qeps_plus - qeps_minus) / (2 * epsilon)))
 
-    def get_action_derivative(self, y_true, y_pred, epsilon=0.2):
-        qeps_plus, _, _ = self.critic.calculate_q_value_on_batch(
-            y_true, y_pred + epsilon)
-        qeps_minus, _, _ = self.critic.calculate_q_value_on_batch(
-            y_true, y_pred - epsilon)
-        return np.float_((qeps_plus - qeps_minus) / (2 * epsilon))
+    def get_state_derivative(self, states, actions, epsilon=0.5):
+        # Need to take derivative for each action separately
+        # e.g. if we have batch size of 5, and 10 actions, we expect an
+        # output for dQ / da of shape (5, 10).
+        grads = np.zeros((self.replay_batch_size, self.state_dim[0]))
 
-    def train_critic(self, states, next_states, actions, rewards, dones):
+        for i in range(self.state_dim[0]):
+            states_tmp1 = np.array(states).copy()
+            states_tmp1[:, i] += epsilon
+            qeps_plus, _, _ = self.critic.calculate_q_value_on_batch(
+                states_tmp1, actions)
+
+            states_tmp2 = np.array(states).copy()
+            states_tmp2[:, i] -= epsilon
+            qeps_minus, _, _ = self.critic.calculate_q_value_on_batch(
+                states_tmp2, actions)
+
+            grads[:, i] = np.atleast_1d(
+                np.float_((qeps_plus - qeps_minus) / (2 * epsilon)))
+        grads = np.asarray(grads, dtype=np.float32)
+        return grads.T
+    # def get_action_derivative(self, y_true, y_pred, epsilon=0.2):
+        # # print('action deriv, plus epsilon')
+        # qeps_plus, _, _ = self.critic.calculate_q_value_on_batch(
+        #     y_true, y_pred + epsilon)
+        #
+        # # print('action deriv, minus epsilon')
+        # qeps_minus, _, _ = self.critic.calculate_q_value_on_batch(
+        #     y_true, y_pred - epsilon)
+        # return np.atleast_1d(
+        #     np.float_((qeps_plus - qeps_minus) / (2 * epsilon)))
+
+    def get_action_derivative(self, states, actions, epsilon=0.4):
+        # Need to take derivative for each action separately
+        # e.g. if we have batch size of 5, and 10 actions, we expect an
+        # output for dQ / da of shape (5, 10).
+        grads = np.zeros((self.replay_batch_size, self.action_n))
+
+        for i in range(self.action_n):
+            # print('qeps plus')
+            actions_tmp1 = np.array(actions).copy()
+            actions_tmp1[:, i] += epsilon
+            qeps_plus, _, _ = self.critic.calculate_q_value_on_batch(
+                states, actions_tmp1)
+
+            # print('qeps minus')
+            actions_tmp2 = np.array(actions).copy()
+            actions_tmp2[:, i] -= epsilon
+            qeps_minus, _, _ = self.critic.calculate_q_value_on_batch(
+                states, actions_tmp2)
+
+            grad_ = np.float_((qeps_plus - qeps_minus) / (2 * epsilon))
+            grads[:, i] = grad_.flatten()
+
+        grads = np.asarray(grads, dtype=np.float32)
+        # print('grads', grads)
+        return grads.T
+
+    def get_state_action_derivative_analytical(self, states, actions):
+        q_values, _, _, grads_wrt_s, grads_wrt_a = \
+            self.critic.calculate_q_value_on_batch(
+                states, actions, calc_derivative=True)
+
+        # We need to flip the sign since this is the derivative wrt F,
+        # but we want derivative wrt. Q, where Q = -F
+        grads_wrt_a *= -1
+        grads_wrt_s *= -1
+
+        return q_values, grads_wrt_s.T, grads_wrt_a.T
+
+    def get_analytical_action_derivative(self, states, actions):
+        grads = np.zeros((self.replay_batch_size, self.action_n))
+
+    def get_action_derivative_5point(self, states, actions, epsilon=0.15):
+        # Need to take derivative for each action separately
+        # e.g. if we have batch size of 5, and 10 actions, we expect an
+        # output for dQ / da of shape (5, 10).
+        grads = np.zeros((self.replay_batch_size, self.action_n))
+
+        for i in range(self.action_n):
+            actions_tmp1 = np.array(actions).copy()
+            actions_tmp1[:, i] += epsilon
+            qeps_plus, _, _ = self.critic.calculate_q_value_on_batch(
+                states, actions_tmp1)
+
+            actions_tmp1[:, i] += epsilon
+            q2eps_plus, _, _ = self.critic.calculate_q_value_on_batch(
+                states, actions_tmp1)
+
+            actions_tmp2 = np.array(actions).copy()
+            actions_tmp2[:, i] -= epsilon
+            qeps_minus, _, _ = self.critic.calculate_q_value_on_batch(
+                states, actions_tmp2)
+
+            actions_tmp2[:, i] -= epsilon
+            q2eps_minus, _, _ = self.critic.calculate_q_value_on_batch(
+                states, actions_tmp2)
+
+            grad_ = np.float_((-q2eps_plus + 8*qeps_plus - 8*qeps_minus +
+                               q2eps_minus) / (12 * epsilon))
+            grads[:, i] = grad_.flatten()
+
+        grads = np.asarray(grads, dtype=np.float32)
+        # print('grads', grads)
+        return grads.T
+
+
+    def train_critic(self, states, next_states, actions, rewards, dones,
+                     random_phase=False):
         # Training the QBM
         # Use experiences found in replay_buffer to update weights
+        if random_phase:
+            next_actions = []
+            for i in range(len(next_states)):
+                next_actions.append(self.env.action_space.sample())
+        else:
+            next_actions = self.get_target_actions(next_states)
+
+        q_loss_batch = np.zeros(len(states))
         for jj in np.arange(len(states)):
+            # print('self.actor.get_weights()', self.actor.get_weights())
+            # print('self.critic.w_hh', self.critic.w_hh)
+
             # Act only greedily here: should be OK to do that always since we
             # collect our experiences according to epsilon-greedy policy.
 
             # Recalculate q_value of (sample.state, sample.action) pair
+            # TODO: changed critic_target to critic here
             q_value, spin_configs, visible_nodes = (
-                self.critic_target.calculate_q_value(states[jj], actions[jj]))
+                self.critic.calculate_q_value(states[jj], actions[jj]))
 
             # Now calculate the next_q_value of the greedy action, without
             # actually taking the action (to take actions in env.,
             # we don't follow purely greedy action).
-            next_action = self.get_target_actions(next_states[jj])
-            # print('next action', next_action)
+            # next_action = self.get_target_actions(next_states[jj])
+            # next_action = next_action.flatten()
             next_q_value, spin_configurations, visible_nodes = (
                 self.critic_target.calculate_q_value(
-                    state=next_states[jj], action=next_action))
+                    state=next_states[jj], action=next_actions[jj]))
 
             # Update weights and target Q-function if needed
             self.critic.update_weights(
                 spin_configs, visible_nodes, q_value, next_q_value,
                 rewards[jj], learning_rate=self.learning_rate_critic)
+
+            q_target = rewards[jj] + self.critic.small_gamma * next_q_value
+            q_loss = (q_value - q_target) ** 2
+            q_loss_batch[jj] = q_loss
+        self.q_losses.append(q_loss_batch.mean())
+
 
     def _soft_update_actor_and_critic(self):
         """ Perform update of target actor and critic network weights using
@@ -195,6 +352,20 @@ class QuantumActorCritic:
         """
         states, actions, rewards, next_states, dones = (
             self.replay_memory.get_sample(batch_size=self.replay_batch_size))
+        # print('train, states.shape, actions.shape, rewards.shape, '
+        #       'next_states.shape, dones.shape', states.shape, actions.shape,
+        #       rewards.shape, next_states.shape, dones.shape)
+        # rewards *= 100
         self.train_critic(states, next_states, actions, rewards, dones)
+
+        # Calculate Q values before making update on actor
+        # q, _, _ = self.critic.calculate_q_value_on_batch(states, actions)
+        # self.q_before.append(q.mean())
+        # self.mu_losses.append(-q.mean())
         self.train_actor(states, actions)
+        #
+        # actions_tmp = self.actor.predict_on_batch(states)
+        # q, _, _ = self.critic.calculate_q_value_on_batch(states, actions_tmp)
+        # self.q_after.append(q.mean())
+
         self._soft_update_actor_and_critic()
--- qbm_rl_steering/run_actor_critic.py
+++ qbm_rl_steering/run_actor_critic.py
@@ -2,9 +2,11 @@ import numpy as np
 
 from qbm_rl_steering.core.visualization import plot_log
 from qbm_rl_steering.core.qac import QuantumActorCritic
-# from qbm_rl_steering.environment.target_steering_1d import TargetSteeringEnv
 
 from cern_awake_env.simulation import SimulationEnv
+
+from qbm_rl_steering.environment.target_steering_1d import TargetSteeringEnv
+from qbm_rl_steering.environment.target_steering_2d import TargetSteeringEnv2D
 
 try:
     import matplotlib
@@ -13,37 +15,49 @@ try:
 except ImportError as err:
     print(err)
 
+import matplotlib.pyplot as plt
 
-gamma_rl = 0.85
-n_epochs = 16
-max_episode_length = 10
-initial_exploration_steps = 10
+
+gamma_rl = 0.95
+n_epochs = 15
+max_episode_length = 20  #12
+initial_exploration_steps = 50
 initial_reward = 0
+reward_scale = 1
 
 # env = TargetSteeringEnv(max_steps_per_episode=max_episode_length)
 env = SimulationEnv(plane='H', remove_singular_devices=True)
-agent = QuantumActorCritic(env, gamma_rl=gamma_rl)
+# env = TargetSteeringEnv2D(max_steps_per_episode=max_episode_length)
+agent = QuantumActorCritic(env, gamma_rl=gamma_rl, batch_size=16)
 
 state, reward, done, ep_rew, ep_len, ep_cnt = (
     env.reset(), initial_reward, False, [[]], 0, 0)
+reward *= reward_scale
 
 # Calculate reward in current state
-_, intensity = env.get_pos_at_bpm_target(env.mssb_angle)
-ep_rew[-1].append(env.get_reward(intensity))
+# _, intensity = env.get_pos_at_bpm_target(env.mssb_angle, env.mbb_angle)
+# ep_rew[-1].append(env.get_reward(intensity))
+ep_rew[-1].append(reward_scale * env.compute_reward(
+            state, goal=None, info={}))
 
 total_steps = max_episode_length * n_epochs
 
 # Main loop: collect experience in env and update/log each epoch
-to_exploitation = False
+random_action = None
 for t in range(total_steps):
     if t > initial_exploration_steps:
         action = agent.get_action(state, episode=1)
         action = np.squeeze(action)
+        random_action = False
     else:
         action = env.action_space.sample()
+        random_action = True
+        print('sampling random action')
 
     # Step the env
+    print('action', action)
     next_state, reward, done, _ = env.step(action)
+    reward *= reward_scale
     ep_rew[-1].append(reward)  # keep adding to the last element till done
     ep_len += 1
 
@@ -57,44 +71,99 @@ for t in range(total_steps):
     if done or (ep_len == max_episode_length):
         ep_cnt += 1
         if True:
-            print(f"Episode: {len(ep_rew) - 1}, Reward: {ep_rew[-1][-1]}, "
+            print(f"Episode: {len(ep_rew) - 1}, Reward initial: "
+                  f"{ep_rew[-1][0]}, "
+                  f"Reward final:"
+                  f" {ep_rew[-1][-1]}, "
                   f"Length: {len(ep_rew[-1])}")
         ep_rew.append([])
 
         for _ in range(ep_len):
+            # if random_action:
+            #     # Train only QBM Q net for the random action phase
+            #     states, actions, rewards, next_states, dones = (
+            #         agent.replay_memory.get_sample(
+            #             batch_size=agent.replay_batch_size))
+            #     agent.train_critic(states, next_states, actions, rewards,
+            #                        dones, random_phase=True)
+            # else:
+            #     # Once we are out of the random action phase, train actor and
+            #     # critic. Idea is that Q net already has been trained in the
+            #     # right direction.
             agent.train()
 
         state, reward, done, ep_ret, ep_len = (
             env.reset(), initial_reward, False, 0, 0)
+        reward *= reward_scale
 
-        _, intensity = env.get_pos_at_bpm_target(env.mssb_angle)
-        ep_rew[-1].append(env.get_reward(intensity))
+        # _, intensity = env.get_pos_at_bpm_target(env.mssb_angle, env.mbb_angle)
+        # ep_rew[-1].append(env.get_reward(intensity))
+        ep_rew[-1].append(reward_scale * env.compute_reward(
+            state, goal=None, info={}))
 
+# Extract logging data
 init_rewards = []
 rewards = []
 reward_lengths = []
 for episode in ep_rew[:-1]:
-    if (len(episode) > 0):
+    if len(episode) > 0:
         rewards.append(episode[-1])
         init_rewards.append(episode[0])
         reward_lengths.append(len(episode) - 1)
 print('Total number of interactions:', np.sum(reward_lengths))
 
-plot_log(env, fig_title='Training')
+fig, axs = plt.subplots(2, 1, sharex=True)
+axs[0].plot(reward_lengths)
+axs[1].plot(init_rewards, c='r')
+axs[1].plot(rewards, c='g')
+plt.show()
 
 # Agent evaluation
-n_episodes_eval = 50
+n_episodes_eval = 80
 episode_counter = 0
-env = TargetSteeringEnv(max_steps_per_episode=max_episode_length)
+
+rewards_eval = []
+env = SimulationEnv(plane='H', remove_singular_devices=True)
+# env = TargetSteeringEnv2D(max_steps_per_episode=max_episode_length)
+# env = TargetSteeringEnv(max_steps_per_episode=max_episode_length)
 while episode_counter < n_episodes_eval:
-    state = env.reset(init_outside_threshold=True)
+    state = env.reset()
     state = np.atleast_2d(state)
+    reward_ep = []
+
+    # _, intensity = env.get_pos_at_bpm_target(env.mssb_angle, env.mbb_angle)
+    # if env.get_reward(intensity) > -15:
+    #     continue
+    # print('init reward', env.get_reward(intensity))
+    #
+    # reward_ep.append(env.get_reward(intensity))
+    reward_ep.append(reward_scale * env.compute_reward(state, goal=None,
+                                                       info={}))
+    n_steps_eps = 0
     while True:
         a = agent.get_action(state, noise=0)
         a = np.squeeze(a)
         state, reward, done, _ = env.step(a)
-        if done:
+        reward *= reward_scale
+        reward_ep.append(reward)
+        if done or n_steps_eps > max_episode_length:
             episode_counter += 1
+            rewards_eval.append(reward_ep)
             break
+        n_steps_eps += 1
 
-plot_log(env, fig_title='Evaluation')
+rewards_eval = np.array(rewards_eval)
+
+fig, axs = plt.subplots(2, 1, sharex=True)
+init_rew = np.zeros(len(rewards_eval))
+final_rew = np.zeros(len(rewards_eval))
+length = np.zeros(len(rewards_eval))
+for i in range(len(rewards_eval)):
+    init_rew[i] = rewards_eval[i][0]
+    final_rew[i] = rewards_eval[i][-1]
+    length[i] = len(rewards_eval[i]) - 1
+
+axs[1].plot(init_rew, c='r')
+axs[1].plot(final_rew, c='g')
+axs[0].plot(length)
+plt.show()
