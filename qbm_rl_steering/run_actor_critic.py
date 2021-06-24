81bdf7af381eabda8b430e7cd5b2e6c6818ca437
mischenk <michael.schenk@cern.ch>
2021-06-24 17:27:06 +0200

    Refactor main script running agent training and evaluation

--- qbm_rl_steering/run_actor_critic.py
+++ qbm_rl_steering/run_actor_critic.py
@@ -1,16 +1,15 @@
 import numpy as np
+from tqdm import tqdm
 
-from qbm_rl_steering.core.visualization import plot_log
 from qbm_rl_steering.core.qac import QuantumActorCritic
-
 from cern_awake_env.simulation import SimulationEnv
+# from qbm_rl_steering.environment.target_steering_1d import TargetSteeringEnv
+# from qbm_rl_steering.environment.target_steering_2d import TargetSteeringEnv2D
 
-from qbm_rl_steering.environment.target_steering_1d import TargetSteeringEnv
-from qbm_rl_steering.environment.target_steering_2d import TargetSteeringEnv2D
+import gym
 
 try:
     import matplotlib
-
     matplotlib.use('qt5agg')
 except ImportError as err:
     print(err)
@@ -18,152 +17,176 @@ except ImportError as err:
 import matplotlib.pyplot as plt
 
 
+def trainer(env: gym.Env, agent: QuantumActorCritic, n_episodes: int,
+            max_episode_length: int, exploration_steps: int):
+    """ Run agent-environment interactions for agent training.
+    :param env: openAI gym environment
+    :param agent: RL agent (DDPG), either QuantumActorCritic or classical DDPG
+    :param n_episodes: total number of training episodes
+    :param max_episode_length: maximum number of steps per episode
+    :param exploration_steps: initial number of interactions with env for which
+    agent follows a random policy. After that period, it will follow the
+    current actor policy (including some action noise to keep some level of
+    exploration).
+    :return all_rewards: list of lists containing rewards for all episodes of
+    training.
+    """
+    all_rewards = []
+    episode_count = 0
+    total_step_count = 0
+
+    reset_env = True
+    while episode_count < n_episodes:
+        # Beginning new episode
+        if reset_env:
+            episode_rewards = []
+            state = env.reset()
+            step_count = 0
+            reset_env = False
+
+        # Calculate reward in current state
+        try:
+            episode_rewards.append(
+                env.compute_reward(state, goal=None, info={}))
+        except:
+            _, intensity = env.get_pos_at_bpm_target(
+                env.mssb_angle, env.mbb_angle)
+            episode_rewards.append(env.get_reward(intensity))
+
+        if total_step_count >= exploration_steps:
+            action = agent.get_action(state, episode=1)
+            action = np.squeeze(action)
+            if total_step_count == exploration_steps:
+                print('END OF EXPLORATION PERIOD: following policy action')
+        else:
+            action = env.action_space.sample()
+
+        # Step the env
+        next_state, reward, done, _ = env.step(action)
+        episode_rewards.append(reward)
+        step_count += 1
+        total_step_count += 1
+
+        done = False if (step_count == max_episode_length) else done
+
+        # Store experience to replay buffer
+        agent.replay_memory.store(state, action, reward, next_state, done)
+
+        state = next_state
+
+        # Bookkeeping and training on replay buffer
+        if done or (step_count == max_episode_length):
+            episode_count += 1
+            all_rewards.append(np.array(episode_rewards))
+            print(f"Episode: {episode_count}, "
+                  f"Reward initial: {episode_rewards[0]}, "
+                  f"Reward final: {episode_rewards[-1]}, "
+                  f"Number of steps: {step_count}")
+
+            # Train loop
+            for _ in tqdm(range(step_count)):
+                agent.train()
+            reset_env = True
+
+    return np.array(all_rewards)
+
+
+def evaluator(env: gym.Env, agent: QuantumActorCritic, n_episodes: int,
+              max_episode_length: int):
+    """ Run evaluation for a number of episodes, typically using a trained
+    agent.
+    :param env: openAI gym environment
+    :param agent: RL agent (DDPG), either QuantumActorCritic or classical DDPG
+    :param n_episodes: number of episodes for evaluation
+    :param max_episode_length: maximum number of steps per episode
+    :return all_rewards: list of lists containing rewards for all episodes of
+    training.
+    """
+    episode_count = 0
+
+    all_rewards = []
+    while episode_count < n_episodes:
+        episode_rewards = []
+        state = env.reset()
+
+        # Get reward of the initial state
+        try:
+            episode_rewards.append(env.compute_reward(state, goal=None, info={}))
+        except:
+            _, intensity = env.get_pos_at_bpm_target(
+                env.mssb_angle, env.mbb_angle)
+            episode_rewards.append(env.get_reward(intensity))
+
+        step_count = 0
+        while True:
+            a = agent.get_action(state, noise=0)
+            a = np.squeeze(a)
+            state, reward, done, _ = env.step(a)
+            episode_rewards.append(reward)
+            if done or step_count > max_episode_length:
+                all_rewards.append(np.array(episode_rewards))
+                episode_count += 1
+                break
+            step_count += 1
+    return np.array(all_rewards)
+
+
+def plot_log(all_rewards, plot_title=''):
+    """ Extract initial and final rewards as well as episode lengths and plot
+    them. """
+    initial_rewards = np.zeros(len(all_rewards))
+    final_rewards = np.zeros(len(all_rewards))
+    episode_lengths = np.zeros(len(all_rewards))
+    for i in range(len(all_rewards)):
+        initial_rewards[i] = all_rewards[i][0]
+        final_rewards[i] = all_rewards[i][-1]
+        episode_lengths[i] = len(all_rewards[i]) - 1
+    print(f'Total number of interactions: {np.sum(episode_lengths)}')
+
+    fig, axs = plt.subplots(2, 1, sharex=True)
+    plt.suptitle(plot_title, fontsize=12)
+    axs[0].plot(episode_lengths)
+    axs[1].plot(initial_rewards, c='r', label='Initial')
+    axs[1].plot(final_rewards, c='g', label='Final')
+    axs[1].legend(loc='lower right')
+    axs[0].set_ylabel('Episode length')
+    axs[1].set_ylabel('Reward')
+    axs[1].set_xlabel('Episode')
+    plt.show()
+
+
+# TODO: fix 'issue' that env does not stop immediately when we are already
+#  within threshold?
+
+# ENVIRONMENT
+# env = TargetSteeringEnv(max_steps_per_episode=max_episode_length)
+# env = TargetSteeringEnv2D(max_steps_per_episode=max_episode_length)
+env = SimulationEnv(plane='H', remove_singular_devices=True)
+
+# AGENT
 gamma_rl = 0.95
-n_epochs = 15
-max_episode_length = 20  #12
-initial_exploration_steps = 50
-initial_reward = 0
-reward_scale = 1
+batch_size = 16
+agent = QuantumActorCritic(env, gamma_rl=gamma_rl, batch_size=batch_size)
 
-# env = TargetSteeringEnv(max_steps_per_episode=max_episode_length)
-env = SimulationEnv(plane='H', remove_singular_devices=True)
-# env = TargetSteeringEnv2D(max_steps_per_episode=max_episode_length)
-agent = QuantumActorCritic(env, gamma_rl=gamma_rl, batch_size=16)
+# RUN TRAINING LOOP
+n_episodes = 100
+max_episode_length = 20
+exploration_steps = 50
+training_rewards = trainer(
+    env=env, agent=agent, n_episodes=n_episodes,
+    max_episode_length=max_episode_length, exploration_steps=exploration_steps)
 
-state, reward, done, ep_rew, ep_len, ep_cnt = (
-    env.reset(), initial_reward, False, [[]], 0, 0)
-reward *= reward_scale
+# PLOT TRAINING EVOLUTION
+plot_log(training_rewards, plot_title='Training')
 
-# Calculate reward in current state
-# _, intensity = env.get_pos_at_bpm_target(env.mssb_angle, env.mbb_angle)
-# ep_rew[-1].append(env.get_reward(intensity))
-ep_rew[-1].append(reward_scale * env.compute_reward(
-            state, goal=None, info={}))
-
-total_steps = max_episode_length * n_epochs
-
-# Main loop: collect experience in env and update/log each epoch
-random_action = None
-for t in range(total_steps):
-    if t > initial_exploration_steps:
-        action = agent.get_action(state, episode=1)
-        action = np.squeeze(action)
-        random_action = False
-    else:
-        action = env.action_space.sample()
-        random_action = True
-        print('sampling random action')
-
-    # Step the env
-    print('action', action)
-    next_state, reward, done, _ = env.step(action)
-    reward *= reward_scale
-    ep_rew[-1].append(reward)  # keep adding to the last element till done
-    ep_len += 1
-
-    done = False if ep_len == max_episode_length else done
-
-    # Store experience to replay buffer
-    agent.replay_memory.store(state, action, reward, next_state, done)
-
-    state = next_state
-
-    if done or (ep_len == max_episode_length):
-        ep_cnt += 1
-        if True:
-            print(f"Episode: {len(ep_rew) - 1}, Reward initial: "
-                  f"{ep_rew[-1][0]}, "
-                  f"Reward final:"
-                  f" {ep_rew[-1][-1]}, "
-                  f"Length: {len(ep_rew[-1])}")
-        ep_rew.append([])
-
-        for _ in range(ep_len):
-            # if random_action:
-            #     # Train only QBM Q net for the random action phase
-            #     states, actions, rewards, next_states, dones = (
-            #         agent.replay_memory.get_sample(
-            #             batch_size=agent.replay_batch_size))
-            #     agent.train_critic(states, next_states, actions, rewards,
-            #                        dones, random_phase=True)
-            # else:
-            #     # Once we are out of the random action phase, train actor and
-            #     # critic. Idea is that Q net already has been trained in the
-            #     # right direction.
-            agent.train()
-
-        state, reward, done, ep_ret, ep_len = (
-            env.reset(), initial_reward, False, 0, 0)
-        reward *= reward_scale
-
-        # _, intensity = env.get_pos_at_bpm_target(env.mssb_angle, env.mbb_angle)
-        # ep_rew[-1].append(env.get_reward(intensity))
-        ep_rew[-1].append(reward_scale * env.compute_reward(
-            state, goal=None, info={}))
-
-# Extract logging data
-init_rewards = []
-rewards = []
-reward_lengths = []
-for episode in ep_rew[:-1]:
-    if len(episode) > 0:
-        rewards.append(episode[-1])
-        init_rewards.append(episode[0])
-        reward_lengths.append(len(episode) - 1)
-print('Total number of interactions:', np.sum(reward_lengths))
-
-fig, axs = plt.subplots(2, 1, sharex=True)
-axs[0].plot(reward_lengths)
-axs[1].plot(init_rewards, c='r')
-axs[1].plot(rewards, c='g')
-plt.show()
-
-# Agent evaluation
-n_episodes_eval = 80
-episode_counter = 0
-
-rewards_eval = []
+# RUN EVALUATION
+n_episodes_evaluation = 80
 env = SimulationEnv(plane='H', remove_singular_devices=True)
 # env = TargetSteeringEnv2D(max_steps_per_episode=max_episode_length)
 # env = TargetSteeringEnv(max_steps_per_episode=max_episode_length)
-while episode_counter < n_episodes_eval:
-    state = env.reset()
-    state = np.atleast_2d(state)
-    reward_ep = []
 
-    # _, intensity = env.get_pos_at_bpm_target(env.mssb_angle, env.mbb_angle)
-    # if env.get_reward(intensity) > -15:
-    #     continue
-    # print('init reward', env.get_reward(intensity))
-    #
-    # reward_ep.append(env.get_reward(intensity))
-    reward_ep.append(reward_scale * env.compute_reward(state, goal=None,
-                                                       info={}))
-    n_steps_eps = 0
-    while True:
-        a = agent.get_action(state, noise=0)
-        a = np.squeeze(a)
-        state, reward, done, _ = env.step(a)
-        reward *= reward_scale
-        reward_ep.append(reward)
-        if done or n_steps_eps > max_episode_length:
-            episode_counter += 1
-            rewards_eval.append(reward_ep)
-            break
-        n_steps_eps += 1
+evaluation_rewards = evaluator(
+    env=env, agent=agent, n_episodes=n_episodes_evaluation,
+    max_episode_length=max_episode_length)
 
-rewards_eval = np.array(rewards_eval)
-
-fig, axs = plt.subplots(2, 1, sharex=True)
-init_rew = np.zeros(len(rewards_eval))
-final_rew = np.zeros(len(rewards_eval))
-length = np.zeros(len(rewards_eval))
-for i in range(len(rewards_eval)):
-    init_rew[i] = rewards_eval[i][0]
-    final_rew[i] = rewards_eval[i][-1]
-    length[i] = len(rewards_eval[i]) - 1
-
-axs[1].plot(init_rew, c='r')
-axs[1].plot(final_rew, c='g')
-axs[0].plot(length)
-plt.show()
+plot_log(evaluation_rewards, plot_title='Evaluation')
